187eb69992edffc54bc6387d79993847d0e361bc
Bram van Dooremaal <bram@dooremaal.nl>
2012-03-18 10:59:55 +0100

    added constructor filling.

--- Java_src/src/model/ModelDiagram.java
+++ Java_src/src/model/ModelDiagram.java
@@ -1,10 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
+import controller.DataController;
+
 public class ModelDiagram {
 	int versie;
+	private ArrayList<DiagramObject> dol = new ArrayList<DiagramObject>();
+	
+	
+	private void setVersie(int i) {
+		this.versie = i;
+	}
+	
+	public int getVersie() {
+		return versie;
+	}
 	
 	public ModelDiagram() {
-		
+		DataController dc = DataController.getInstance();
+		this.setVersie(dc.getVersie());
 	}
 	
 	public void draw() {
