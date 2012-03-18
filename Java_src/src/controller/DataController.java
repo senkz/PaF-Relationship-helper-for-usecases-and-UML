723e357da10823cc06168f8793f39c154facb307
Bram van Dooremaal <bram@dooremaal.nl>
2012-03-18 10:59:32 +0100

    added gathering methods, made getInstance static

--- Java_src/src/controller/DataController.java
+++ Java_src/src/controller/DataController.java
@@ -1,21 +1,43 @@
 package controller;
+import java.util.ArrayList;
+
 import model.ModelDiagram;
 
 
 public class DataController {
 	private static DataController instance = null;
-	ModelDiagram md;	// mogelijk meerdere diagrammen?
+	private ArrayList<ModelDiagram> mdl = new ArrayList<ModelDiagram>();
 	
-	private DataController() {
-	}
+	private DataController() {}
 	
-	public DataController getInstance() {
+	public static DataController getInstance() {
 		if(instance == null)
 			instance = new DataController();
 		return instance;
 	}
 	
+	public void addModel(ModelDiagram md) {
+		mdl.add(md);
+	}
+	
 	public ModelDiagram getModel() {
+		return getModel(this.getVersie()-1);
+	}
+	
+	public ModelDiagram getModel(int i) {
+		for(ModelDiagram m : mdl)
+			if(m.getVersie() == i)
+				return m;
 		return null;
+	}
+	
+	public int getVersie() {
+		if(mdl.size() == 0)
+			return 1;
+		int i = 0;
+		for(ModelDiagram m : mdl)
+			if(m.getVersie() > i)
+				i = m.getVersie()+1;
+		return i;
 	}
 }
