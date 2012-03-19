95f4983808e4891fca738cda8e1bdf3d665cea14
Seth Snel <sethsnel@gmail.com>
2012-03-19 13:04:39 +0100

    Export file!!!

--- Java_src/src/GUI.java
+++ Java_src/src/GUI.java
@@ -1,11 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -16,6 +19,7 @@ public class GUI {
 	private JTextField filename = new JTextField(), dir = new JTextField();
 	private JFrame frame = new JFrame();
 	private JFileChooser fileBrowser = new JFileChooser();
+	private JComboBox<String> exportType = new JComboBox<String>();
 
 	public GUI() {
 		frame.setLayout(new BorderLayout());
@@ -39,6 +43,33 @@ public class GUI {
 				}
 			}
 		});
+		
+		exportType.addItem("xmlmd");
+		
+		JLabel chooseExportType = new JLabel("Kies export formaat");
+		
+		JButton exportButton = new JButton("save file");
+		exportButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				int rVal = fileBrowser.showSaveDialog(frame);
+				if (rVal == JFileChooser.APPROVE_OPTION) {
+					filename.setText(fileBrowser.getSelectedFile().getName());
+					dir.setText(fileBrowser.getCurrentDirectory().toString());
+					guic.write(dir.getText()+"\\"+filename.getText(), (String) exportType.getSelectedItem());
+				}
+				if (rVal == JFileChooser.CANCEL_OPTION) {
+					filename.setText("You pressed cancel");
+					dir.setText("");
+				}
+			}
+		});
+		
+		JPanel exportPanel = new JPanel();
+		exportPanel.setLayout(new GridLayout(3,1));
+		exportPanel.add(chooseExportType);
+		exportPanel.add(exportType);
+		exportPanel.add(exportButton);
 
 		JButton reportButton = new JButton("generate report");
 		reportButton.addActionListener(new ActionListener() {
@@ -49,6 +80,7 @@ public class GUI {
 
 		JPanel buttonPannel = new JPanel();
 		buttonPannel.add(readButton);
+		buttonPannel.add(exportPanel);
 		buttonPannel.add(reportButton);
 
 		frame.add(buttonPannel, BorderLayout.NORTH);
